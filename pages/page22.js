0b957b1f7de096290d8a450e45b1d8f47346a3cd
John Carlisle <johncarlislework@gmail.com>
2023-09-12 21:39:31 +1000

    12 Sept23

--- pages/page22.js
+++ pages/page22.js
@@ -1,9 +1,5 @@
 
 
-
-
-
-
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { Button, Input } from 'antd';
@@ -19,9 +15,6 @@ export default function FormComponent() {
   const [isFetching, setIsFetching] = useState(false);
   const [isPageRefreshed, setIsPageRefreshed] = useState(false);
   const [fetchedData, setFetchedData] = useState([]);
-  const [one, two] = useState([]);
-
-
 
   const onSubmit = async ({ forminput1 }) => {
     try {
@@ -38,73 +31,69 @@ export default function FormComponent() {
       const data1 = await response.json();
       setResult(data1.result);
       setIsFetching(false);
-
-      await submitToMongoDB(result);
-      
     } catch (error) {
-      
       setIsFetching(false);
     }
   };
 
   const submitToMongoDB = async () => {
     try {
-      const response1 = await fetch('./api/mongapi', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ result }),
-      });
-      const resdata = await response1.json();
+      if (result !== null) { // Check if result is not null before submitting
+        const response1 = await fetch('./api/mongapi', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ result }),
+        });
+        const resdata = await response1.json();
 
-      // Handle the response if needed
+        // Handle the response if needed
+      }
     } catch (error) {
-      
+      // Handle the error
     }
   };
 
   useEffect(() => {
     async function fetchData() {
       try {
-        // First, post data to MongoDB
-        await submitToMongoDB(result);
+        // First, post data to MongoDB if result is not null
+        if (result !== null) {
+          await submitToMongoDB();
+        }
 
         // Then, fetch data from MongoDB
         const response = await fetch('./api/mongget');
         const data = await response.json();
         setFetchedData(data);
-        console.log('=============================================')
+
+        // Process the data
         const two = [];
         const three = [];
         const one = data.map(function(item){
             two.push(item.result)
         })
 
-
-
-var leng = 19
-console.log(two.length)
-
-var cut = two.splice(leng,1)
-for(var i=0; i<two.length; i++){
-
-  
-}
-
-three.push(cut)
-
-        console.log(three)
-
-      
-     //   await submitgenerateWord(fetchedData)
+        var leng = two.length-1
+       
+        
+        var cut = two.splice(leng,1);
+        for(var i=0; i<two.length; i++){
+          // Do something with the data
+        }
+        
+        three.push(cut);
+        console.log(three);
+        
       } catch (error) {
         console.error(error);
       }
     }
-  
+
     // Call the fetchData function when the component mounts
     fetchData();
   }, [result]);
 
+  // Function to insert double line breaks before the last line
   const finfin = function insertDoubleLineBreakBeforeLastLine(text) {
     if (text) { // Add a null check
       const lines = text.split('.');
@@ -115,11 +104,6 @@ three.push(cut)
       return ''; // Return an empty string if text is null
     }
   }
-
- 
-
-  
-
 
   return (
     <div className="">
@@ -132,26 +116,18 @@ three.push(cut)
 
       {fetchedData && (
         <div className="">
-          
-
-      <div className="bg-black text-white text-3xl">
-        {fetchedData.length >= 4 && (
-          <div
-            dangerouslySetInnerHTML={{
-              __html: finfin(fetchedData[fetchedData.length - 1].result),
-            }}
-          />
-        )}
-      </div>
-
-
- 
+          <div className="bg-black text-white text-3xl">
+            {fetchedData.length >= 4 && (
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: finfin(fetchedData[fetchedData.length - 1].result),
+                }}
+              />
+            )}
+          </div>
         </div>
       )}
-
-
     </div>
   );
 }
-
 
